f973d0e625b9d4bb42cf9b26c9b7bf794f8f8789
Anirudh-Kyatham <114123968+Anirudh-Kyatham@users.noreply.github.com>
2022-10-10 02:20:06 -0400

    Add files via upload

---
+++ Question_4.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<cmath>
+//complexity O👎
+//as the inner while loop runs n times only if it is the correct substring
+
+
+int lengthOfLongestSubstring(std::string s) {
+    std::unordered_map<char, int> datastore;
+    int start{ 0 }, end{ 0 };
+    int res{ 0 };
+
+    while (end < s.length())
+    {
+        char ch_end = s[end];
+        datastore[ch_end]++;
+
+        while (datastore[ch_end] > 1) {
+            char ch_start = s[start];
+            datastore[ch_start]--;
+            start++;
+        }
+        res = std::max(res, end - start + 1);
+        end++;
+    }
+    return res;
+
+}
+
+int main(int argc, char const* argv[])
+{
+    std::string s{ "abcabcbb" };
+    int res = lengthOfLongestSubstring(s);
+
+    std::cout << res << std::endl;
+
+    return 0;
+}
